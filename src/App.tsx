a869017988ed10e8990c9c31af1340f31ef8bf7a
At1z <Adrianlistkiewiczv2@gmail.com>
2025-01-05 17:21:26 +0100

    zadanie 3 solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports.ts";
+import { withAuthenticator, useAuthenticator } from "@aws-amplify/ui-react";
+Amplify.configure(awsExports);
 
-export default function App() {
+function App() {
+  const { signOut } = useAuthenticator((context) => [context.signOut]);
   return (
     <div className="container">
       <h1 className="heading">Hello World</h1>
+      <button className="sign-out-button" onClick={signOut}>
+        Sign Out
+      </button>
     </div>
   );
 }
+export default withAuthenticator(App);
