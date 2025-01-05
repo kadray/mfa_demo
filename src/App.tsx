7cd0f04736f945c9b0728cfba746c4c024e2ac02
At1z <Adrianlistkiewiczv2@gmail.com>
2025-01-05 17:12:39 +0100

    zadanie 2 solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports.ts";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-export default function App() {
+Amplify.configure(awsExports);
+
+function App() {
   return (
     <div className="container">
       <h1 className="heading">Hello World</h1>
     </div>
   );
 }
+export default withAuthenticator(App);
